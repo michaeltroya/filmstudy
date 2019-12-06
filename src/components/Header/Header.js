031897bf5c8b2fc38c6014dbb1a1776486b00434
Michael <michaeltroya13@gmail.com>
2019-12-06 13:06:51 -0500

    add custom logo and favicon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
+import Logo from '../../images/logo.png';
+
+import { Row, Col, Container } from 'react-bootstrap';
+
 function Header() {
   return (
     <header className="header">
-      <h1>HELLO</h1>
-      <h6>THIS IS FILMSTUDY HELOO</h6>
+      <Container>
+        <Row>
+          <Col className="header-col" xs={12} md={6}>
+            <img src={Logo} alt="logo" className="header-logo" />
+          </Col>
+          <Col className="header-col" xs={12} md={6}>
+            <div className="how-to">
+              <h5>How to</h5>
+              <h6> 1. Select movies you've watched </h6>
+              <h6> 2. Press the "Go" button </h6>
+              <h6> 3. Check out what we've learned about your selections </h6>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </header>
   );
 }
